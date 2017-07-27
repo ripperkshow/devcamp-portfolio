a1922c91482251259bfca5af793f6219a751c431
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 15:57:35 +0000

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
-      "".html_safe +
+      " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
@@ -44,20 +44,31 @@ module ApplicationHelper
       },
     ]
   end
-  
+
   def nav_helper style, tag_type
     nav_links = ''
-    
+
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
-
     nav_links.html_safe
-  end 
-  
+  end
+
   def active? path
     "active" if current_page? path
   end
-    
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Kayden Corbett Portfolio", sticky: false)
+  end
+
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,10 +5,9 @@ class Portfolio < ApplicationRecord
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   validates_presence_of :title, :body
-  
+
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
-
 
   def self.angular
     where(subtitle: 'Angular')
@@ -19,7 +18,4 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-
-  def set_defaults
-  end
 end
